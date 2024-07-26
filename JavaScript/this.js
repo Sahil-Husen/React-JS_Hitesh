97455ad146b7570e9db0f8393add28401209434b
Sahil-husen <sahilhussainsahil76@gmail.com>
2024-07-26 22:34:02 +0530

    JavaScript Concept

---
+++ JavaScript/this.js
@@ -0,0 +1,43 @@
+// // 
+
+// let greeting  ='hello world'
+// for (const greet of greeting) {
+//     if(greet == ' ')
+//     {
+//         continue;
+       
+//     }
+
+//     console.warn(greet);
+ 
+// }
+
+
+
+// let arr = [1,2,3,4]
+//  let ans = arr.reduce((acc,cur)=>{
+//     console.log(`acc:${acc}, cur: ${cur} `);
+//     return acc+cur;
+// },0)
+// console.log(ans);
+
+// let arr =[ 1,2,3,4]
+// let ans = arr.reduce((acc,cur)=> acc + cur ,0)
+// console.log(ans)
+
+// let shopingCart = [
+//     {
+//         item:'js course',
+//         price:2999
+//     },
+//     {
+//         item:'py course',
+//         price:1999
+//     },
+//     {
+//         item:'app dev course',
+//         price:10999
+//     },
+// ]
+// let totalPrice = shopingCart.reduce((acc,item) =>acc + item.price,0)
+// console.log(totalPrice);
