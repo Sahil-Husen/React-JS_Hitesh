4074aae8312e12fc63e1feb4a67304b55f017dac
Sahil-husen <sahilhussainsahil76@gmail.com>
2024-08-07 20:44:55 +0530

    Dice Game Added

--- Random Dice/script.js
+++ Random Dice/script.js
@@ -1,34 +1,30 @@
-// let button = document.querySelector("button");
-
-// button.addEventListener("click", () => {
-//   let images = document.querySelectorAll("img");
-//   let randomNumbers = [];
-//   let randomImageSource = [];
-
-//   for (let i = 0; i < 2; i++) {
-//     randomNumbers[i] = Math.floor(Math.random() * 6 + 1);
-    
-//     randomImageSource[i] = `Images/${randomNumbers[i]}.png`;
-//     // console.log(randomImageSource);
-//     images[i].setAttribute("src", randomImageSource[i]);
- 
-//   }
-
-//   let resultText;
-//   if (randomNumbers[0] < randomNumbers[1]) {
-//     resultText = "Player 1 wins!";
-//   } else if (randomNumbers[0] > randomNumbers[1]) {
-//     resultText = "Player 2 wins!";
-//   } else {
-//     resultText = "It's a tie!";
-//   }
-
-//   document.querySelector("h1").textContent = resultText;
-// });
-
-
 let button = document.querySelector("button");
-button.addEventListener('click',()=>{
-    let images = document.querySelectorAll('img');
+
+button.addEventListener("click", () => {
+  let images = document.querySelectorAll("img");
+  let randomNumbers = [];
+  let randomImageSource = [];
+
+  for (let i = 0; i < 2; i++) {
+    randomNumbers[i] = Math.floor(Math.random() * 6 + 1);
     
-})
+    randomImageSource[i] = `Images/${randomNumbers[i]}.png`;
+    // console.log(randomImageSource);
+    images[i].setAttribute("src", randomImageSource[i]);
+ 
+  }
+
+  let resultText;
+  if (randomNumbers[0] < randomNumbers[1]) {
+    resultText = "Player 1 wins!";
+  } else if (randomNumbers[0] > randomNumbers[1]) {
+    resultText = "Player 2 wins!";
+  } else {
+    resultText = "It's a tie!";
+  }
+
+  document.querySelector("h1").textContent = resultText;
+});
+
+// All set
+ 
