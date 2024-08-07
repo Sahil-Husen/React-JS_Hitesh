a1fde42d51fb81e5c73defed8ae160d854f5037c
Sahil-husen <sahilhussainsahil76@gmail.com>
2024-08-07 11:27:25 +0530

    Todo App

---
+++ Todo/index.js
@@ -0,0 +1,36 @@
+// const select = document.querySelector("select");
+  
+// const para = document.querySelector("p");
+
+// select.addEventListener("change", setValue);
+ 
+
+// function setValue() {
+//   const choose = select.value;
+//   console.log(choose);
+//   switch (choose) {
+//     case "Sunny":
+//       console.log(choose);
+//       para.textContent = "It is sunny today!";
+//       break;
+//     case "Spring":
+//       para.textContent = "It is Spring season";
+//       break;
+
+//     case "Cold":
+//       para.textContent = "It is too cold";
+//       break;
+//     case'Too hot':
+//     para.textContent = 'It is too hot';
+//     break;
+    
+//     case 'Roam':
+//       para.textContent = 'Lets go for roam'
+//       break;
+//     default:
+//       para.textContent = ' ';
+//   }
+// }
+
+ 
+ 
